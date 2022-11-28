4ad2021c511997abb733495786630b3ea1ac20f7
Alena Bushkina <aa-bushkina@mail.ru>
2022-11-29 02:11:54 +0300

    fix organizations table

--- application/src/main/resources/db/V2022.11.29.01.57__create_tabel_organizations.sql
+++ application/src/main/resources/db/V2022.11.29.01.57__create_tabel_organizations.sql
@@ -1,7 +1,7 @@
 CREATE TABLE organizations
 (
     id   SERIAL      NOT NULL,
-    name VARCHAR     NOT NULL,
+    name VARCHAR     NOT NULL UNIQUE,
     inn  VARCHAR(10) NOT NULL UNIQUE,
     CONSTRAINT organization_pk PRIMARY KEY (id),
     CONSTRAINT check_inn CHECK (inn ~ '\d{10}' )
