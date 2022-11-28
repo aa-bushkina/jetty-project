e78a20e67ecdcfc06df381236376fd24a8e4ffb4
Alena Bushkina <aa-bushkina@mail.ru>
2022-11-28 16:13:23 +0300

    fix build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,43 +1,28 @@
 plugins {
-    java
-    `kotlin-dsl`
-    application
-    id("nu.studer.jooq") version "8.0" apply false
+    id("java")
 }
 
-allprojects {
-    repositories {
-        mavenCentral()
-        google()
-    }
+group = "ru.vk"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
 }
 
-subprojects {
+dependencies {
+    implementation("org.eclipse.jetty:jetty-servlet:11.0.12")
+    implementation("org.eclipse.jetty:jetty-server:11.0.12")
+    implementation("org.flywaydb:flyway-core:9.7.0")
+    implementation("org.postgresql:postgresql:42.5.0")
+    implementation("org.jetbrains:annotations:23.0.0")
+    implementation("com.google.inject:guice:5.1.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
+}
 
-    apply {
-        plugin("org.jetbrains.kotlin.jvm")
-        plugin("nu.studer.jooq")
-    }
-
-    dependencies {
-        implementation("com.zaxxer:HikariCP:5.0.1")
-
-        implementation("org.flywaydb:flyway-core:9.6.0")
-        implementation("org.postgresql:postgresql:42.5.0")
-        implementation("org.jetbrains:annotations:23.0.0")
-        implementation("com.google.inject:guice:5.1.0")
-        testImplementation("org.hamcrest:hamcrest:2.2")
-
-
-        implementation("org.jooq:jooq:3.17.4")
-        implementation("org.jooq:jooq-codegen:3.17.4")
-        implementation("org.jooq:jooq-meta:3.17.4")
-
-        testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-    }
-
-    tasks.getByName<Test>("test") {
-        useJUnitPlatform()
-    }
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
 }
