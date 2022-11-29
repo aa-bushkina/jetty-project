419713cb4c68cc60c45625f10ed3c12f0aa7e13f
Alena Bushkina <aa-bushkina@mail.ru>
2022-11-30 00:54:20 +0300

    add only get filter

--- application/src/main/java/ru/vk/application/Application.java
+++ application/src/main/java/ru/vk/application/Application.java
@@ -1,17 +1,21 @@
 package ru.vk.application;
 
 import com.google.inject.Inject;
+import jakarta.servlet.DispatcherType;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.resource.Resource;
 import org.jetbrains.annotations.NotNull;
 import ru.vk.Main;
 import ru.vk.server.MyServer;
+import ru.vk.server.OnlyGetFilter;
 import ru.vk.server.ProductServlet;
 
 import java.net.URL;
+import java.util.EnumSet;
 
 public class Application {
   @NotNull
@@ -38,6 +42,8 @@ public class Application {
 
   public void startServer() throws Exception {
     final Server server = myServer.build();
+    final OnlyGetFilter onlyGetFilter = new OnlyGetFilter();
+    final FilterHolder filterGetHolder = new FilterHolder(onlyGetFilter);
 
     final var context = new ServletContextHandler();
     context.setContextPath("/");
@@ -50,6 +56,7 @@ public class Application {
     context.setWelcomeFiles(new String[]{"/helpInfo.html"});
     context.addServlet(
       new ServletHolder("servlet-help", DefaultServlet.class), "/");
+    context.addFilter(filterGetHolder, "/", EnumSet.of(DispatcherType.REQUEST));
 
     server.setHandler(context);
     server.start();
---
+++ application/src/main/java/ru/vk/server/OnlyGetFilter.java
@@ -0,0 +1,31 @@
+package ru.vk.server;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@SuppressWarnings("NotNullNullableValidation")
+public final class OnlyGetFilter implements Filter {
+
+  @Override
+  public void init(FilterConfig filterConfig) {
+  }
+
+  @Override
+  public void doFilter(ServletRequest request,
+                       ServletResponse response,
+                       FilterChain chain) throws IOException, ServletException {
+    final HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+    if (httpServletRequest.getMethod().equals("GET")) {
+      chain.doFilter(request, response);
+    } else {
+      ((HttpServletResponse) response).sendError(HttpServletResponse.SC_BAD_REQUEST);
+    }
+  }
+
+  @Override
+  public void destroy() {
+  }
+}
